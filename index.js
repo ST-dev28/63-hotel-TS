c7493e3db5fcee710e91e96404eca054d5fd8f8e
SandraT <sndrbltr@gmail.com>
2021-10-09 12:55:51 +0000

    Add room

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
 "use strict";
 class Hotel {
-    constructor(name, address, stars) {
+    constructor(name = "Heaven", address = "Karkle, Placio str. 24", stars = 5) {
         this.name = name;
         this.address = address;
         this.stars = stars;
-        //this.rooms = rooms;
+        this.rooms = [];
+    }
+    addRoom(room) {
+        this.rooms.push(room);
+    }
+    printRooms(minComfort = 15) {
+        console.log(`Here is a list of rooms:`, this.rooms);
     }
     printData() {
         const hotelInfo = `${this.stars} star hotel "${this.name}", loacated in ${this.address}, is waiting for guests!`;
         console.log(hotelInfo);
-        //console.log(`Rooms are: ${this.rooms}.`);
+        console.log(`Here is a list of rooms:`, this.rooms);
     }
 }
-const hotel = new Hotel("Heaven", "Karkle, Placio str. 24", 5);
+const hotel = new Hotel();
 hotel.printData();
 console.log('-------------------');
+var RoomType;
+(function (RoomType) {
+    RoomType[RoomType["minComfort"] = 14] = "minComfort";
+    RoomType[RoomType["double"] = 25] = "double";
+    RoomType[RoomType["luxurySpa"] = 60] = "luxurySpa";
+})(RoomType || (RoomType = {}));
 class Room {
-    constructor(size, capacity, comfort) {
+    //public readonly type: RoomType;
+    constructor(size = 14 | 25 | 60, 
+    //type: RoomType = RoomType.minComfort,
+    capacity = 1 | 2 | 4, comfort) {
         this.size = size;
+        //this.type = type;
         this.capacity = capacity;
         this.comfort = comfort = Math.round(this.size / this.capacity * 10) / 10;
     }
     printData() {
-        console.log(`Room size is ${this.size}m2 => ${this.capacity} person/room, ${this.comfort} m2/person.`);
+        console.log(`Room size is ${this.size}m2 => ${this.capacity} person / room, ${this.comfort} m2 / person.`);
     }
 }
-const room = new Room(15, 2, 7);
+const room = new Room(14, 1, 0);
+const room1 = new Room(25, 2, 0);
+const room2 = new Room(60, 4, 0);
 room.printData();
+hotel.addRoom(room);
+hotel.addRoom(room1);
+hotel.addRoom(room2);
 console.log('-------------------');
 class Spa extends Room {
     constructor(poolSize, poolTemp) {
-        super(100, 4, 20);
+        super(100, 4, 0);
         this.poolSize = poolSize;
         this.poolTemp = poolTemp;
         this.comfort = Math.round((this.size - this.poolSize) / this.capacity * 10) / 10;
     }
     printData() {
         super.printData();
-        console.log(`Pool size is ${this.poolSize}m2 and water temperature is upto ${this.poolTemp}^C`);
+        console.log(`Pool size is ${this.poolSize}m2 and water temperature is upto ${this.poolTemp} ^ C`);
     }
 }
 const spa = new Spa(20, 45);
 spa.printData();
-var addRoom;
-(function (addRoom) {
-    addRoom[addRoom["single"] = 15] = "single";
-    addRoom[addRoom["double"] = 20] = "double";
-    addRoom[addRoom["family"] = 35] = "family";
-    addRoom[addRoom["luxuryPool"] = 100] = "luxuryPool";
-})(addRoom || (addRoom = {}));
--- index.ts
+++ index.ts
@@ -1,50 +1,70 @@
 class Hotel {
     public readonly name: string;
     public readonly address: string;
-    public readonly stars: number;
-    public readonly rooms: [];
+    public readonly stars: number
+    public rooms: Room[];
 
-    public constructor(name: string,
-        address: string,
-        stars: number) {
+    public constructor(name: string = "Heaven",
+        address: string = "Karkle, Placio str. 24",
+        stars: number = 5) {
 
         this.name = name;
         this.address = address;
         this.stars = stars;
-        //this.rooms = rooms;
+        this.rooms = [];
+    }
+
+    public addRoom(room: Room): void {
+        this.rooms.push(room);
+    }
+
+    private printRooms(minComfort: number = 15): void {
+        console.log(`Here is a list of rooms:`, this.rooms);
     }
 
     public printData(): void {
         const hotelInfo = `${this.stars} star hotel "${this.name}", loacated in ${this.address}, is waiting for guests!`
         console.log(hotelInfo);
-        //console.log(`Rooms are: ${this.rooms}.`);
+        console.log(`Here is a list of rooms:`, this.rooms);
     }
 }
-
-const hotel = new Hotel("Heaven", "Karkle, Placio str. 24", 5);
+const hotel = new Hotel();
 hotel.printData();
 console.log('-------------------');
+
+enum RoomType {
+    minComfort = 14,
+    double = 25,
+    luxurySpa = 60
+}
 
 class Room {
     public readonly size: number;
     public readonly capacity: number;
     public readonly comfort: number;
+    //public readonly type: RoomType;
 
-    public constructor(size: number,
-        capacity: number,
+    public constructor(size: number = 14 | 25 | 60,
+        //type: RoomType = RoomType.minComfort,
+        capacity: number = 1 | 2 | 4,
         comfort: number) {
 
         this.size = size;
+        //this.type = type;
         this.capacity = capacity;
         this.comfort = comfort = Math.round(this.size / this.capacity * 10) / 10;
     }
     public printData(): void {
-        console.log(`Room size is ${this.size}m2 => ${this.capacity} person/room, ${this.comfort} m2/person.`);
+        console.log(`Room size is ${this.size}m2 => ${this.capacity} person / room, ${this.comfort} m2 / person.`);
     }
 }
-const room = new Room(15, 2, 7);
+const room = new Room(14, 1, 0);
+const room1 = new Room(25, 2, 0);
+const room2 = new Room(60, 4, 0);
 room.printData();
-
+hotel.addRoom(room);
+hotel.addRoom(room1);
+hotel.addRoom(room2);
 console.log('-------------------');
 
 class Spa extends Room {
@@ -54,24 +74,15 @@ class Spa extends Room {
 
     constructor(poolSize: number,
         poolTemp: number) {
-        super(100, 4, 20);
+        super(100, 4, 0);
         this.poolSize = poolSize;
         this.poolTemp = poolTemp;
         this.comfort = Math.round((this.size - this.poolSize) / this.capacity * 10) / 10;
     }
     public printData(): void {
         super.printData();
-        console.log(`Pool size is ${this.poolSize}m2 and water temperature is upto ${this.poolTemp}^C`);
+        console.log(`Pool size is ${this.poolSize}m2 and water temperature is upto ${this.poolTemp} ^ C`);
     }
 }
 const spa = new Spa(20, 45);
 spa.printData();
-
-enum addRoom {
-    single = 15,
-    double = 20,
-    family = 35,
-    luxuryPool = 100
-}
-
-
